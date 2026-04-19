42dd33cc6994d0e40d44a371af8a166d881127c6
yegor-gondhal <ygondhal@gmail.com>
2026-04-19 13:23:52 -0400

    Update

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 #import cupy as cp
 import numpy as np
-import OpenGL.GL as gl
+from OpenGL.GL import *
 from OpenGL import GLU
-from PyQt5 import QtCore
-from PyQt5 import QtGui
-from PyQt5 import QtOpenGL
+from PyQt6 import QtCore
+from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtOpenGLWidgets import QOpenGLWidget
+from PyQt6 import QtOpenGL
+from PyQt6.QtCore import QTimer
 import sys
 
-class MainWindow(QtGui.QWindow):
+xp = np
+
+class GLWidget(QOpenGLWidget):
     def __init__(self):
-        QtGui.QWindow.__init__(self)
-        self.resize(600, 600)
-        self.setWindowTitle("Quantum Simulation")
+        super().__init__()
+        self.w = 800
+        self.h = 800
+        self.pixel_data = xp.zeros((self.w, self.h, 4), dtype=np.uint8)
+        self.center = xp.zeros(2)
 
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update_sim)
+        self.timer.start(16)
 
-if __name__ == '__main__':
-    app = QtGui.QGuiApplication(sys.argv)
-    win = MainWindow()
-    win.show()
-    sys.exit(app.exec_())
+    def initializeGL(self):
+        glClearColor(0, 0, 0, 1)
+
+        self.texture = glGenTextures(1)
+        glBindTexture(GL_TEXTURE_2D, self.texture)
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+
